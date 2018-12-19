a8e0cb31d2622a16fbfe8d74d2878319556db654
amohajerani <36096580+amohajerani@users.noreply.github.com>
2018-12-19 14:58:38 -0500

    Update and rename label_functions.py to inspection_folder.py

---
+++ inspection_folder.py
@@ -0,0 +1,15 @@
+# Inspect every image in img_dir
+
+import os
+
+import inspection
+
+# Update this path if necessary
+img_dir = 'C:\\Users\\10134838\\Desktop\\DEC19\\images'
+
+img_lst = [os.path.join(img_dir,f) for f in os.listdir(img_dir)]
+img_lst = list(filter(os.path.isfile, img_lst))
+
+
+for img in img_lst:  
+    inspection.inspect(img)
